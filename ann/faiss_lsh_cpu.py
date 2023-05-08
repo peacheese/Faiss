125936196e4cab7d9fa314ab4b5aeb196b611b93
ganqy <735990477@qq.com>
2023-05-08 15:42:57 +0800

    update ann test

--- ann/faiss_lsh_cpu.py
+++ ann/faiss_lsh_cpu.py
@@ -8,15 +8,14 @@ if __name__ == '__main__':
     dim = 128
     x = int(sys.argv[1])
     k = int(sys.argv[2])
-    lsh_k = int(sys.argv[3])
-    print('{}M items, Top-K = {}, LSH-K = {}'.format(x, k, lsh_k))
+    print('{}M items, Top-K = {}'.format(x, k))
     total_num = x * 1024000
 
-    np.random.seed(1234)
+    # np.random.seed(1234)
     corpus = np.random.randint(0, 256, (total_num, dim)).astype('float32')
     query  = np.random.randint(0, 256, (1, dim)).astype('float32')
 
-    index = faiss.IndexLSH(dim, lsh_k)
+    index = faiss.IndexLSH(dim, 4 * dim)
 
     index.add(corpus)
     
--- ann/faiss_lsh_single_gpu.py
+++ ann/faiss_lsh_single_gpu.py
@@ -8,17 +8,16 @@ if __name__ == '__main__':
     dim = 128
     x = int(sys.argv[1])
     k = int(sys.argv[2])
-    lsh_k = int(sys.argv[3])
-    print('{}M items, Top-K = {}, LSH-K = {}'.format(x, k, lsh_k))
+    print('{}M items, Top-K = {}'.format(x, k))
     total_num = x * 1024000
 
     gpu_res = faiss.StandardGpuResources()
 
-    np.random.seed(1234)
+    # np.random.seed(1234)
     corpus = np.random.randint(0, 256, (total_num, dim)).astype('float32')
     query  = np.random.randint(0, 256, (1, dim)).astype('float32')
 
-    index = faiss.IndexLSH(dim, lsh_k)
+    index = faiss.IndexLSH(dim, dim * 2)
     gpu_index = faiss.index_cpu_to_gpu(gpu_res, 0, index)
 
     gpu_index.add(corpus)
@@ -28,3 +27,14 @@ if __name__ == '__main__':
     end_time = time.time()
     run_time = end_time - start_time
     print('Running time : ' + str(run_time) + 's')
+
+    acc_index = faiss.IndexFlatIP(dim)
+    gpu_acc_index = faiss.index_cpu_to_gpu(gpu_res, 0, acc_index)
+    gpu_acc_index.add(corpus)
+    distance, acc_idx = gpu_acc_index.search(query, k)
+    
+    count = 0
+    for item in idx[0]:
+        if item in acc_idx[0]:
+            count += 1
+    print(count)
